aade1a82e77185d49e0b5edd83b9d3d4b773a612
Wiebe Nieuwenhuis <Wiebe@MBP-van-Wiebe.fritz.box>
2016-02-15 17:51:26 +0100

    remove moved files

--- lights.php
+++
@@ -1,9 +0,0 @@
-<?php	
-	include('config.php');
-	include('inc/functions.php');
-	if($_GET['turn'] == 'on'){
-		setValue($light['id'].'/turnOn');
-	} elseif($_GET['turn'] == 'off'){
-		setValue($light['id'].'/turnOff');
-	}
-?>
--- set_temp.php
+++
@@ -1,7 +0,0 @@
-<?php	
-	include('config.php');
-	include('inc/functions.php');
-	if(isset($_GET['temp'])){
-    	setValue($therm['id'].'/changeTemperatureTo?temperatureSetpoint='.$_GET['temp']);				
-	}
-?>
